c3813a72bc77d861b029a72fff0514385a3c8ecf
Egor Sergeev <egor.sergeev@jetbrains.com>
2024-09-21 21:03:47 +0300

    Refactor counter app page structure and add requirements

--- e2e/pages/counter_app.py
+++ e2e/pages/counter_app.py
@@ -3,20 +3,21 @@ from selenium.webdriver.remote.webdriver import WebDriver
 
 
 class CounterPage:
+    COUNTER_VALUE = (By.ID, "counter")
+    PLUS_BUTTON = (By.XPATH, "//button[text()='+']")
+    MINUS_BUTTON = (By.XPATH, "//button[text()='-']")
+
     def __init__(self, driver: WebDriver):
         self.driver = driver
-        self.counter_value = (By.ID, "counter")
-        self.plus_button = (By.XPATH, "//button[text()='+']")
-        self.minus_button = (By.XPATH, "//button[text()='-']")
 
     def load(self, url):
         self.driver.get(url)
 
     def get_counter_value(self):
-        return int(self.driver.find_element(*self.counter_value).text)
+        return int(self.driver.find_element(*self.COUNTER_VALUE).text)
 
     def click_plus(self):
-        self.driver.find_element(*self.plus_button).click()
+        self.driver.find_element(*self.PLUS_BUTTON).click()
 
     def click_minus(self):
-        self.driver.find_element(*self.minus_button).click()
+        self.driver.find_element(*self.MINUS_BUTTON).click()
